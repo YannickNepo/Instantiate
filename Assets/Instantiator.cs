08f2f5330dbb054b0caebfc9f818db02213f1eb8
YannickNepo <101335474+YannickNepo@users.noreply.github.com>
2022-05-09 12:10:04 -0300

    Boton clona

---
+++ Assets/Instantiator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject objectToClone;
+
+    public int cantidad;
+    public void CloneObject()
+
+    {
+        int i = 0;
+        while (i < cantidad)
+        {
+            Instantiate(objectToClone);
+            i++;
+        }
+     }
+   
+}
---
+++ Assets/ShowMessajeInConsole.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMessajeInConsole : MonoBehaviour
+{
+    public void ShowMessaje()
+    {
+        Debug.Log("Hello world");
+    }
+}
